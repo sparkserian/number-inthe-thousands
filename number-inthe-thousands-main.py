164f0c884b2f1b8f1bd46b468cb5f1e251303827
William F. Awuku Jr <33478637+sparkserian@users.noreply.github.com>
2021-11-15 21:54:29 -0500

    Create number-inthe-thousands-main.py
    using the int literal from the variable balance,
    write a program to print output of the variable balance in
    two decimal places and group them in thousands

---
+++ number-inthe-thousands-main.py
@@ -0,0 +1,11 @@
+balance = 2000000.567849
+
+"""using the int literal from the variabe balance, 
+write a program to print output of the variable balance in
+two decimal places and group them in thousands"""
+
+def main():
+    print(f"{balance:,.2f}")
+    
+
+main()
